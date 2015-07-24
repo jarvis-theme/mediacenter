16ad24c1e4ee57d7d78dd3ec1b4968bf9a94a4ba
yogaswara <gistha.yogaswara@gmail.com>
2015-07-24 14:10:56 +0700

    change file to cdn

--- partials/defaultcss.blade.php
+++ partials/defaultcss.blade.php
@@ -1,7 +1,7 @@
 {{favicon()}} 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
 
-{{generate_theme_css('mediacenter/assets/css/bootstrap.min.css')}}
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
 @if($tema->isiCss=='')
 {{generate_theme_css('mediacenter/assets/css/main.css')}}
 @else
@@ -10,6 +10,5 @@
 {{generate_theme_css('mediacenter/assets/css/owl.carousel.css')}}
 {{generate_theme_css('mediacenter/assets/css/owl.transitions.css')}}
 {{generate_theme_css('mediacenter/assets/css/animate.min.css')}}
-{{generate_theme_css('mediacenter/assets/css/config.css')}}
-{{generate_theme_css('mediacenter/assets/css/font-awesome.min.css')}}
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 {{generate_theme_css('mediacenter/assets/css/prettyPhoto.css')}}
